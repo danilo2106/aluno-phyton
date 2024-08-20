f42c184009501e23a0b079ecceb565dab7641f0e
danilo <pedrosodanilo770@gmail.com>
2024-08-20 10:54:29 -0400

    commit 2
    phyton

---
+++ aluno.py
@@ -0,0 +1,28 @@
+print("olá, mundo!")
+x = 5
+y = 2
+igual = x + y
+igual = x - y
+igual = x * y
+igual = x / y
+igual = x // y
+igual = x % y
+igual = x ** y
+
+print(igual)
+
+soma = x + y
+subtracao = x - y
+multiplicacao = x * y
+divisao = x / y
+divisao_inteira = x // y 
+resto_divisao = x % y
+exponenciacao = x ** y
+
+print(soma)
+print(subtracao)
+print(multiplicacao)
+print(divisao)
+print(divisao_inteira)
+print(resto_divisao)
+print(exponenciacao)
