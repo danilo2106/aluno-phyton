6e49c174558987b75cdda7c4f49f69e151b469b9
danilo <pedrosodanilo770@gmail.com>
2024-08-29 11:20:12 -0400

    jogo teste 2

--- aula5 parte3.py
+++ aula5 parte3.py
@@ -1,5 +1,5 @@
 nome_ususario = input("enter username:")
-print("olá,"+ nome_ususario + "como voce esta")
+print("olá,"+ nome_ususario + "como você está")
 print("SENAC ARSENAL...")
 print("vamos jogar um jogo?")
 while True:
@@ -15,7 +15,37 @@ print("pateta mouse\n minie mouse\n ana mouse\n")
 mone = input("resposta/")
 if mone == ("minie mouse"):
  print("voce acertou")
+ print("vamos continuar")
 if mone == ("pateta mouse"):
      print("voce errou")
 if mone == ("ana mouse"):
+    print("voce errou") 
+print("quanto é 50 + 320")
+print("345\n 370\n 390\n")
+reg = input("resposta/")
+if reg ==("370"):
+    print("voce acertou")
+if reg ==("345"):
+    print("voce errou")
+if reg ==("390"):
+    print("voce errou")
+    
+print("quanto é 150 x 15")
+print("2250\n 3450\n 2550\n")
+more = input("resposta/")
+if more ==("2250"):
+    print("voce acertou")
+if more ==("3450"):
+    print("voce errou")
+if more ==("2550"):
+    print("voce errou")
+
+print("qual o nome do maior oceano do planeta terra?")
+print("atlântico\n pacífico\n índico\n")
+kl = input("resposta/")
+if kl ==("atlântico"):
+    print("voce errou")
+if kl ==("pacífico"):
+    print("voce acertou")
+if kl ==("índico"):
     print("voce errou")
